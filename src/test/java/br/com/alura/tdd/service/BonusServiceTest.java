e2b19a5e93674e1323b2d1d41f79aaddf2431e00
itzmyhub <italo.machado@mail.uft.edu.br>
2023-06-21 23:54:43 -0300

    lidando com exceções

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -13,9 +13,7 @@ class BonusServiceTest {
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioAcimaDeDezMil() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Julio", LocalDate.now(), new BigDecimal("25000")));
-
-        assertEquals((new BigDecimal("0.00")), bonus);
+        assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(new Funcionario("Julio", LocalDate.now(), new BigDecimal("25000"))));
     }
 
     @Test
